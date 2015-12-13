ce50bd534af1d6c14c1eb16ad04978c198ff4f2f
stravas <stravas.dicasta@gmail.com>
2015-12-13 01:21:10 +0100

    Anpassung an Zellklasse

--- TicTacToe 2.0/Zelle.cs
+++ TicTacToe 2.0/Zelle.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
 
 namespace TicTacToe_2._0
 {
     class Zelle
     {
+
+        public Zelle()
+        {
+
+            this.Rectangle = new Rectangle();
+
+        }
+
+        public Rectangle Rectangle;
         private Spieler _Spieler;
-        private int _X;
-        private int _Y;
-        private int _Hoehe;
-        private int _Breite;
 
         public Spieler Spieler
         {
@@ -22,36 +28,6 @@ namespace TicTacToe_2._0
             get { return this._Spieler; }
         }
 
-        public int x
-        {
-            //setze Spieler
-            set { this._X = value; }
-            //hole Spieler
-            get { return this._X; }
-        }
-
-        public int y
-        {
-            //setze Spieler
-            set { this._Y = value; }
-            //hole Spieler
-            get { return this._Y; }
-        }
-
-        public int hoehe
-        {
-            //setze Spieler
-            set { this._Hoehe = value; }
-            //hole Spieler
-            get { return this._Hoehe; }
-        }
-
-        public int breite
-        {
-            //setze Spieler
-            set { this._Breite = value; }
-            //hole Spieler
-            get { return this._Breite; }
-        }
+        //TODO: setDimensionRectangle
     }
 }
